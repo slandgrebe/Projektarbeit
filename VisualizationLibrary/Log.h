9f85fe4825f2723f2c2a0804c087d2680e5f9e0d
slandgrebe <stefan.landgrebe@gmail.com>
2014-04-13 14:39:37 +0200

    kleine änderung

--- VisualizationLibrary/Log.h
+++ VisualizationLibrary/Log.h
@@ -132,7 +132,7 @@ inline std::string NowTime()
 	char result[100] = { 0 };
 	static DWORD first = GetTickCount();
 	//std::sprintf(result, "%s.%03ld", buffer, (long)(GetTickCount() - first) % 1000);
-	sprintf_s(result, "%s.%03ld", buffer, (long)(GetTickCount() - first) % 1000);
+	sprintf_s(result, "%s.%03ld", buffer, (long)(GetTickCount64() - first) % 1000);
 	return result;
 }
 
