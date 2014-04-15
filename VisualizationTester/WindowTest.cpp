de6f30d5db76957ff1a6b7afcd48c53615f1259c
slandgrebe <stefan.landgrebe@gmail.com>
2014-04-15 12:49:21 +0200

    ohne google test projekt

---
+++ VisualizationTester/WindowTest.cpp
@@ -0,0 +1,6 @@
+#include "WindowTest.h"
+
+
+WindowTest::WindowTest()
+{
+}
---
+++ VisualizationTester/WindowTest.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <gtest\gtest.h>
+
+class WindowTest : public ::testing::Test {
+public:
+	WindowTest();
+
+
+};
+
+TEST(WindowTest, openWindow) {
+	
+}
