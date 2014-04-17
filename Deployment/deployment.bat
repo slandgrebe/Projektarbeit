d60903f9adc52a23be44e0dc8e204489a2de9c8e
slandgrebe <stefan.landgrebe@gmail.com>
2014-04-17 22:38:31 +0200

    Deployment Batch File

---
+++ Deployment/deployment.bat
@@ -0,0 +1,74 @@
+:7-zip verzeichnis ersetzen
+@echo off
+set sevenZip=F:\Programme\Tools\7-Zip\7z.exe
+
+:Eingabe
+@echo off 
+set /P version=d(Debug) oder r(Release):
+IF %version% == d GOTO Debug
+IF %version% == r GOTO Release
+IF 1==1 GOTO Fail
+
+:*******************************************
+:Release
+set source=Release
+set target=Projektarbeit
+
+:aufraeumen
+del %0\..\%target%.exe /F /Q
+del %0\..\%target% /F /Q
+rmdir %0\..\%target%\
+
+:dateien kopieren
+mkdir %0\..\%target%
+
+COPY %0\..\..\%source%\Visualization.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Controller.exe %0\..\%target%\ /Y
+COPY %0\..\..\%source%\VisualizationExample.exe %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Model.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\View.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Assimp32.dll %0\..\%target%\ /Y
+
+mkdir %0\..\%target%\data
+XCOPY %0\..\..\%source%\data %0\..\%target%\data /S /Y
+
+mkdir "%0\..\%target%\Resource Files"
+XCOPY %0\..\..\%source%\data "%0\..\%target%\Resource Files" /S /Y
+
+:selbstentpackendes archiv erstellen
+"%sevenZip%" a %target%.exe -mmt -mx5 -sfx7z.sfx "%0\..\%target%"
+
+:*******************************************
+:Debug
+:7-zip verzeichnis ersetzen
+set sevenZip=F:\Programme\Tools\7-Zip\7z.exe
+set source=Debug
+set target=Projektarbeit_Debug
+:set source=Release
+:set target=Projektarbeit
+
+:aufraeumen
+del %0\..\%target%.exe /F /Q
+del %0\..\%target% /F /Q
+rmdir %0\..\%target%\
+
+:dateien kopieren
+mkdir %0\..\%target%
+
+COPY %0\..\..\%source%\Visualization.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Controller.exe %0\..\%target%\ /Y
+COPY %0\..\..\%source%\VisualizationExample.exe %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Model.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\View.dll %0\..\%target%\ /Y
+COPY %0\..\..\%source%\Assimp32.dll %0\..\%target%\ /Y
+
+mkdir %0\..\%target%\data
+XCOPY %0\..\..\%source%\data %0\..\%target%\data /S /Y
+
+mkdir "%0\..\%target%\Resource Files"
+XCOPY %0\..\..\%source%\data "%0\..\%target%\Resource Files" /S /Y
+
+:selbstentpackendes archiv erstellen
+"%sevenZip%" a %target%.exe -mmt -mx5 -sfx7z.sfx "%0\..\%target%"
+
+:Fail
